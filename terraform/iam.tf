6b3f30dcf1e8f9ebb237f5c3416bb3237f64ed94
Rehap1 <rehabhamed806@gmail.com>
2026-07-24 19:27:15 +0300

    feat: deploy TaskAPI to EKS with ALB ingress

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -92,3 +92,60 @@ resource "aws_iam_role_policy_attachment" "ebs_csi" {
   role       = aws_iam_role.ebs_csi.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
+
+
+### AWS Load Balancer Controller IAM Role
+
+data "aws_iam_policy_document" "load_balancer_controller_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["pods.eks.amazonaws.com"]
+    }
+
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession"
+    ]
+  }
+}
+
+resource "aws_iam_role" "load_balancer_controller" {
+  name = "${local.cluster_name}-load-balancer-controller-role"
+
+  assume_role_policy = data.aws_iam_policy_document.load_balancer_controller_assume_role.json
+
+  tags = {
+    Name = "${local.cluster_name}-load-balancer-controller-role"
+  }
+}
+
+resource "aws_iam_policy" "load_balancer_controller" {
+  name        = "${local.cluster_name}-load-balancer-controller-policy"
+  description = "Permissions for the AWS Load Balancer Controller"
+
+  policy = file("${path.module}/aws-load-balancer-controller-policy.json")
+
+  tags = {
+    Name = "${local.cluster_name}-load-balancer-controller-policy"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "load_balancer_controller" {
+  role       = aws_iam_role.load_balancer_controller.name
+  policy_arn = aws_iam_policy.load_balancer_controller.arn
+}
+
+resource "aws_eks_pod_identity_association" "load_balancer_controller" {
+  cluster_name    = aws_eks_cluster.main.name
+  namespace       = "kube-system"
+  service_account = "aws-load-balancer-controller"
+  role_arn        = aws_iam_role.load_balancer_controller.arn
+
+  depends_on = [
+    aws_iam_role_policy_attachment.load_balancer_controller,
+    aws_eks_addon.pod_identity_agent
+  ]
+}
